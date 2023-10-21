efb2e4ed232fc7cabdd284a62a7ce21881ae5d22
Omer4ruq <omerfaruq4@gmail.com>
2023-10-21 22:26:49 +0600

    business partner page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,19 +7,21 @@ const Contact = () => {
         className="hero min-h-screen"
         style={{
           backgroundImage:
-            "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+            "url(https://i.ibb.co/MsBMHYJ/5226399-mountain-mountain-range-sunlight-orange-cloudy-cloud-purple-snow-glow-peak-movement-light-la.jpg)",
         }}
       >
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <h1 className="mb-5 text-5xl font-bold">Why US?</h1>
             <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              At our website, we offer a wide range of stylish men's fashion,
+              exceptional customer experience, competitive pricing, and a
+              commitment to quality. We're your one-stop shop for fashion that
+              meets your needs and enhances your style. Shop with us to discover
+              the difference."
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            <button className="btn btn-primary">Start Business Acount</button>
           </div>
         </div>
       </div>
